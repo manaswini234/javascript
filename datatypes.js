7c27fbc232559af749955cb74999d814e4ab2d7c
manaswini234 <madirajumanaswini8686@gmail.com>
2026-02-26 22:44:00 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+
+
+let a=10
+let m="manaswini madiraju"
+let t=true
+let u
+let s=null
+console.log(a,m,t,u,s);
+console.log(typeof(a));
+console.log(typeof(m));
+console.log(typeof(t));
+console.log(typeof(u));
+console.log(typeof(s));
+
+let h=25
+h="manu"
+console.log(typeof(h))
+
