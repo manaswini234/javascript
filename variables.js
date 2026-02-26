5de0485cc354ebc947a232f2dcbe47e7f23845e1
manaswini234 <madirajumanaswini8686@gmail.com>
2026-02-26 22:15:58 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,23 @@
+var n="manaswini";
+n=20;
+console.log(n);
+
+let m=5;
+m="manu";
+console.log(m);
+
+const v=true;
+console.log(v);
+
+console.log(typeof(n));
+console.log(typeof(m));
+console.log(typeof(v));
+
+if(n==20){
+    let a=12;
+    console.log(a);
+}
+
+
+
+
