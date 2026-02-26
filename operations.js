8c4bac13ea1663fec4d84525824fef7a752ea613
manaswini234 <madirajumanaswini8686@gmail.com>
2026-02-26 23:07:57 +0530

    op

---
+++ operations.js
@@ -0,0 +1,21 @@
+let a=4;let b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+if(a>=b){
+    console.log("first no greater")
+}
+if(a<=b){
+    console.log("first no lesser")
+}
+if(a==b){
+    console.log("equal")
+}
+let p=10
+let q="10"
+console.log(p===q)
+console.log(p==q)
+console.log("10" - 2);
+console.log("10" + 2);
